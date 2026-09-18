8d875c4964b1353f72a9af722627fa292fc068c4
Kaleab <kaleabayenew519@gmail.com>
2026-09-18 07:28:21 +0300

    tghjk

--- controllers/aiController.js
+++ controllers/aiController.js
@@ -1,11 +1,11 @@
 import { AiConfigModel } from '../models/aiConfigModel.js'
 
 export const AiController = {
-  getConfig(req, res) {
+  async getConfig(req, res) {
     res.json({ ok: true, data: await AiConfigModel.get() })
   },
 
-  updateConfig(req, res) {
+  async updateConfig(req, res) {
     const { aiEnabled } = req.body
     if (typeof aiEnabled !== 'boolean') {
       return res.status(400).json({ ok: false, error: 'aiEnabled must be a boolean' })
