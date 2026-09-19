058a5e4276011bc357728b10ae20405ef601a3db
Kaleab <kaleabayenew519@gmail.com>
2026-09-19 12:48:27 +0300

    crfvgbhnjmkl

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ const PORT = process.env.PORT || process.env.BACKEND_PORT || 10000
 const HARDCODED_ORIGINS = [
   'http://localhost:3000',
   'http://127.0.0.1:3000',
+  'http://localhost:3001',
+  'http://127.0.0.1:3001',
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'https://tic-tak-5qd1.onrender.com',
